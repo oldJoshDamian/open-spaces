7165354ddc2819157316646d8edaf58fbd898135
joshDamian <akachijc@gmail.com>
2021-10-11 12:19:03 +0100

    testing ipfs

---
+++ resources/views/view-resource.blade.php
@@ -0,0 +1,3 @@
+<x-app-layout>
+    <iframe src="https://ipfs.io/ipfs/{{ $resource }}" height="700px" width="100%"></iframe>
+</x-app-layout>
--- routes/web.php
+++ routes/web.php
@@ -20,6 +20,10 @@ Route::get('/spaces/{space}', ['App\Http\Controllers\SpaceController', 'show'])-
 Route::get('/spaces/{space}/concept/{concept}', ['App\Http\Controllers\ConceptController', 'show'])->name('concept.show');
 Route::get('/spaces/{space}/concept/{concept}/topic/{topic}', ['App\Http\Controllers\TopicController', 'show'])->name('topic.show');
 
+Route::get('/resource-viewer/{resource}', function($resource) {
+    return view('view-resource', compact('resource'));
+});
+
 Route::middleware(['auth:sanctum', 'verified'])->group(function () {
     Route::get('/dashboard', function () {
         return view('dashboard');
