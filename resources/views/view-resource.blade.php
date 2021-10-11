9ae37d94870dd5e779cbc2bea65f2a07eb5750c5
joshDamian <akachijc@gmail.com>
2021-10-11 17:22:55 +0100

    testing ipfs

--- resources/views/view-resource.blade.php
+++ resources/views/view-resource.blade.php
@@ -1,5 +1,5 @@
 <x-app-layout>
-    <div id="adobe-dc-view"></div>
+    <div class="h-screen" id="adobe-dc-view"></div>
     <script src="https://documentcloud.adobe.com/view-sdk/main.js"></script>
     <script type="text/javascript">
         document.addEventListener("adobe_dc_view_sdk.ready", function() {
