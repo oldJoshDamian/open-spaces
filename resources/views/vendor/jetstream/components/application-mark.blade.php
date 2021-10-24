052ec42935efebf8b85bd92dc0096010caf6d766
Avoaja Joshua Akachi <akachijc@gmail.com>
2021-10-24 21:43:56 +0000

    Update application-mark.blade.php

--- resources/views/vendor/jetstream/components/application-mark.blade.php
+++ resources/views/vendor/jetstream/components/application-mark.blade.php
@@ -1,1 +1,1 @@
-<img src="{{ asset('icon/open spaces - logo _ edited.png') }}" class="object-scale-down object-center w-16 h-16" />
+<img src="/icon/open spaces - logo _ edited.png" class="object-scale-down object-center w-16 h-16" />
