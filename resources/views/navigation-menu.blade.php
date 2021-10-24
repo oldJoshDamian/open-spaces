a3e7e3bdb5843a6f2fe2dbd1af833e76a59d7d93
Avoaja Joshua Akachi <akachijc@gmail.com>
2021-10-24 21:43:00 +0000

    Update navigation-menu.blade.php

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -7,7 +7,7 @@
                 <div class="flex items-center flex-shrink-0">
                     <a class="flex items-center p-2 text-2xl font-bold no-underline bg-white border-8 border-blue-600 border-double rounded-full toggleColour hover:no-underline lg:text-4xl" href="{{ route('home') }}">
                         <!--Icon from: http://www.potlabicons.com/ -->
-                        <img src="{{ asset('icon/open spaces - logo _ edited.png') }}" class="object-scale-down object-center w-9 h-9" />
+                        <img src="/icon/open spaces - logo _ edited.png" class="object-scale-down object-center w-9 h-9" />
                     </a>
                 </div>
 
