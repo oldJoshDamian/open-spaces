66d3c60c242af648fb83ea6ea327e72f290f4678
Avoaja Joshua Akachi <akachijc@gmail.com>
2021-10-27 16:48:52 +0100

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -292,58 +292,26 @@
         <div class="container px-8 mx-auto">
             <div class="flex flex-col w-full py-6 md:flex-row">
                 <div class="flex-1 mb-6 text-black md:mr-3">
-                    <a class="text-2xl font-bold text-blue-800 no-underline hover:no-underline lg:text-4xl" href="#">
+                    <a class="text-2xl font-bold text-blue-800 no-underline hover:no-underline lg:text-4xl" href="/">
                         {{ config('app.name') }}
                     </a>
                 </div>
-                <div class="flex-1">
-                    <p class="text-gray-500 uppercase md:mb-6">Links</p>
-                    <ul class="mb-6 list-reset">
-                        <li class="inline-block mt-2 mr-2 md:block md:mr-0">
-                            <a href="#" class="text-gray-800 no-underline hover:underline hover:text-pink-500">FAQ</a>
-                        </li>
-                        <li class="inline-block mt-2 mr-2 md:block md:mr-0">
-                            <a href="#" class="text-gray-800 no-underline hover:underline hover:text-pink-500">Help</a>
-                        </li>
-                        <li class="inline-block mt-2 mr-2 md:block md:mr-0">
-                            <a href="#" class="text-gray-800 no-underline hover:underline hover:text-pink-500">Support</a>
-                        </li>
-                    </ul>
-                </div>
-                <div class="flex-1">
-                    <p class="text-gray-500 uppercase md:mb-6">Legal</p>
-                    <ul class="mb-6 list-reset">
-                        <li class="inline-block mt-2 mr-2 md:block md:mr-0">
-                            <a href="#" class="text-gray-800 no-underline hover:underline hover:text-pink-500">Terms</a>
-                        </li>
-                        <li class="inline-block mt-2 mr-2 md:block md:mr-0">
-                            <a href="#" class="text-gray-800 no-underline hover:underline hover:text-pink-500">Privacy</a>
-                        </li>
-                    </ul>
-                </div>
+                
+                
                 <div class="flex-1">
                     <p class="text-gray-500 uppercase md:mb-6">Social</p>
                     <ul class="mb-6 list-reset">
+                        
                         <li class="inline-block mt-2 mr-2 md:block md:mr-0">
-                            <a href="#" class="text-gray-800 no-underline hover:underline hover:text-pink-500">Facebook</a>
+                            <a href="https://github.com/joshDamian/open-spaces" class="text-gray-800 no-underline hover:underline hover:text-pink-500">Github</a>
                         </li>
-                        <li class="inline-block mt-2 mr-2 md:block md:mr-0">
-                            <a href="#" class="text-gray-800 no-underline hover:underline hover:text-pink-500">Linkedin</a>
-                        </li>
-                        <li class="inline-block mt-2 mr-2 md:block md:mr-0">
-                            <a href="#" class="text-gray-800 no-underline hover:underline hover:text-pink-500">Twitter</a>
-                        </li>
+                        
                     </ul>
                 </div>
                 <div class="flex-1">
                     <p class="text-gray-500 uppercase md:mb-6">Company</p>
                     <ul class="mb-6 list-reset">
-                        <li class="inline-block mt-2 mr-2 md:block md:mr-0">
-                            <a href="#" class="text-gray-800 no-underline hover:underline hover:text-pink-500">Official Blog</a>
-                        </li>
-                        <li class="inline-block mt-2 mr-2 md:block md:mr-0">
-                            <a href="#" class="text-gray-800 no-underline hover:underline hover:text-pink-500">About Us</a>
-                        </li>
+                        
                         <li class="inline-block mt-2 mr-2 md:block md:mr-0">
                             <a href="mailto:akachijc@gmail.com" class="text-gray-800 no-underline hover:underline hover:text-pink-500">Contact</a>
                         </li>
